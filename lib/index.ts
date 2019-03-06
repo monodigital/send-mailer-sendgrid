afc26b266ce743675a4d4032b1eb303942d0ca2b
Giuseppe Cutuli <giuseppe@monodigital.studio>
2019-03-06 12:49:21 +0100

    fix package.json and index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,8 @@
 /** Modules **/
 export { MailerModule } from './mailer.module';
 
-/** Adapters **/
-export { PugAdapter } from './adapters/pug.adapter';
-export { HandlebarsAdapter } from './adapters/handlebars.adapter';
-
 /** Interfaces **/
 export { MailerOptions } from './interfaces/mailer-options.interface';
-export { TemplateAdapter } from './interfaces/template-adapter.interface';
 export { MailerOptionsFactory } from './interfaces/mailer-options-factory.interface';
 
 /** Services **/
